5d73e0dda6423176dc56953607f707aadb68577d
Steven Reitsma <steven@properchaos.nl>
2015-05-19 19:54:08 +0200

    Clean up.

--- algorithm.py
+++ algorithm.py
@@ -26,10 +26,10 @@ class EA(object):
 		self.crossover_rate = 0.7
 	
 	def initialize_random_population(self):
-		return np.random.randint(2, size = (10000, 1000)).astype(theano.config.floatX) # 1000 entities, 1000 bits
+		return np.random.randint(2, size = (1000, 1000)).astype(theano.config.floatX) # 1000 entities, 1000 bits
 
 class SimpleEA(EA):
-	def __init__(self, fitnessFunction, selectionFunction):
+	def __init__(self, fitnessFunction):
 		"""
 		Initialize a simple EA algorithm class.
 		Creating the SimpleEA object compiles the needed Theano functions.
@@ -40,9 +40,8 @@ class SimpleEA(EA):
 		super(SimpleEA, self).__init__()
 
 		self.fitness = fitnessFunction
-		self.selection = selectionFunction
 	
-	def cross_alt(self, entities):
+	def cross(self, entities):
 		n, m = entities.shape
 		pop = T.reshape(entities, (2, n*m/2))
 		xpoints = self.rng.random_integers(size = (n / 2,), low = 0, high = m-1)
@@ -50,80 +49,31 @@ class SimpleEA(EA):
 		def choice_vector(xpoint, nbits):
 			return T.concatenate([T.zeros((xpoint,), dtype='uint8'), T.ones((nbits-xpoint,), dtype='uint8')])
 		
-		values, updates = theano.map(fn=choice_vector, sequences=[xpoints], non_sequences=[m], name='Choice Vector')
+		values, updates = theano.map(fn=choice_vector, sequences=[xpoints], non_sequences=[m], name='choice_vector_building')
 		
 		a = T.reshape(values, (n*m/2,))
 		pop = T.concatenate([T.choose(a, pop), T.choose(1-a, pop)])
 		pop = T.reshape(pop, (n,m))
 		return pop
 
-	def cross(self, entities):
+	def tournament_selection(self, entities, fitness, rng):
 		"""
 		TODO:
-		* Really slow, optimization needed.
+		* Contains 1 HostFromGpu (argmax)
 		"""
-		def single_crossover(i, r_i, xpoint, entities):
-			if T.gt(r_i, 0):
-				# Parents
-				e1 = entities[i]
-				e2 = entities[i-1]
+		# Create random integers
+		r = rng.random_integers(size = (entities.shape[0]*2,), low = 0, high = entities.shape[0]-1)
 
-				# Children
-				e3 = T.concatenate([e1[:xpoint], e2[xpoint:]])
-				e4 = T.concatenate([e2[:xpoint], e1[xpoint:]])
+		# Take randomly matched entities' fitnesses and reshape
+		chosen_f = fitness[r].reshape((2, entities.shape[0]))
 
-				# Replace parents with children
-				new_e = T.stack(e3, e4)
-				return {entities: T.set_subtensor(entities[i-1:i+1], new_e)}
-			else:
-				return {}
-		
-		def single_crossover2(i, r_i, xpoint, entities):
-			if T.gt(r_i, 0):
-				# Parents
-				w = entities.shape[1]
-				p = entities[[i-1,i]]
-				s = T.concatenate([T.zeros((xpoint,), dtype='int32'), T.ones((w-xpoint,), dtype='int32')])
-				s2 = 1 - s
-				c = T.stack(T.choose(s,p), T.choose(s2,p))
-				return {entities: T.set_subtensor(entities[[i-1,i]], c)}
-			else:
-				return {}
+		# Take winner IDs (binary)
+		winner_ids = T.argmax(chosen_f, axis=0)
 
-		entity_pair_range = T.arange(1, entities.shape[0], 2)
+		# Compute entity IDs
+		entity_ids = r[T.arange(entities.shape[0])+(winner_ids*entities.shape[0])]
 
-		# Generate crossover bools once to save calls, use as sequence
-		r = self.rng.choice(size = (entities.shape[0] / 2,), p = [1-self.crossover_rate, self.crossover_rate])
-
-		# Generate crossover points once
-		xpoints = self.rng.random_integers(size = (entities.shape[0] / 2,), low = 0, high = entities.shape[1]-1)
-	
-		values, updates = theano.scan(fn=single_crossover, sequences=[entity_pair_range, r, xpoints], non_sequences=[entities], name="crossover", profile=theano.config.profile)
-
-		return updates[entities]
-
-	def mutation(self, entities):
-		"""
-		TODO:
-		* Results not as good as fast_mutation, while this function is more intuitive, why?
-		* Slow, need to optimize.
-		"""
-		def single_mutate(i, entities):
-			e = entities[i]
-			r = self.rng.choice(size = (1,), p = [1-self.entity_mutate_rate, self.entity_mutate_rate])
-
-			# Mutate entity with certain probability
-			if T.gt(r, 0):
-				# Generate random bits
-				rb = self.rng.choice(size = (entities.shape[1],), p = [1-self.bit_mutate_rate, self.bit_mutate_rate])
-
-				# Flip random bits
-				e = T.set_subtensor(e[rb.nonzero()], (e[rb.nonzero()] + 1) % 2)
-
-			return e
-
-		entities, _ = theano.map(fn = single_mutate, sequences=[T.arange(entities.shape[0])], non_sequences=[entities])
-		return entities
+		return entities[entity_ids]
 
 	def fast_mutation(self, entities):
 		"""
@@ -157,9 +107,9 @@ class SimpleEA(EA):
 
 		# Create graphs
 		fitness_t = self.fitness(E)
-		select_t = self.selection(E, F, self.rng)
+		select_t = self.tournament_selection(E, F, self.rng)
 		mutate_t = self.fast_mutation(E)
-		crossover_t = self.cross_alt(E)
+		crossover_t = self.cross(E)
 
 		# Compile functions
 		fitness = theano.function([], [], updates = [(F, fitness_t)])
@@ -191,10 +141,10 @@ class SimpleEA(EA):
 		end = time.time()
 
 		if theano.config.profile:
-			theano.printing.pydotprint(fitness, outfile="fitness.png")
-			theano.printing.pydotprint(select, outfile="select.png")
-			theano.printing.pydotprint(mutate, outfile="mutate.png")
-			theano.printing.pydotprint(crossover, outfile="cross.png", scan_graphs = True)
+			theano.printing.pydotprint(fitness, outfile="fitness.pdf", format="pdf")
+			theano.printing.pydotprint(select, outfile="select.pdf", format="pdf")
+			theano.printing.pydotprint(mutate, outfile="mutate.pdf", format="pdf")
+			theano.printing.pydotprint(crossover, outfile="cross.pdf", scan_graphs = True, format="pdf")
 
 		return E.eval(), start, end, i+1
 		
@@ -203,26 +153,7 @@ if __name__ == "__main__":
 	def fit(entities):
 		return T.sum(entities, axis=1)
 
-	def sel(entities, fitness, rng):
-		"""
-		TODO:
-		* Contains 1 HostFromGpu (argmax)
-		"""
-		# Create random integers
-		r = rng.random_integers(size = (entities.shape[0]*2,), low = 0, high = entities.shape[0]-1)
-
-		# Take randomly matched entities' fitnesses and reshape
-		chosen_f = fitness[r].reshape((2, entities.shape[0]))
-
-		# Take winner IDs (binary)
-		winner_ids = T.argmax(chosen_f, axis=0)
-
-		# Compute entity IDs
-		entity_ids = r[T.arange(entities.shape[0])+(winner_ids*entities.shape[0])]
-
-		return entities[entity_ids]
-
-	ea = SimpleEA(fit, sel)
+	ea = SimpleEA(fit)
 
 	if len(sys.argv) > 1 and sys.argv[1] == "benchmark":
 		times = 20
